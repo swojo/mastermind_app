c2be7942e1dce6ed00415db3625bde24d35c68cc
Samantha <wojtowi4@illinois.email>
2014-07-17 14:12:56 -0500

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 MastermindApp::Application.routes.draw do
+  root 'static_pages#home'
+  get '/rules' => 'static_pages#rules', as: 'rules'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  subject{page}
+
+  describe "Home" do
+    before {visit root_path }
+
+    it {should have_content('Mastermind Game') }
+  
+    describe "Click Play" do
+      before{ click_button "Play Here!" }   
+
+      it {should have_content('Play Mastermind') }
+    end
+  
+  end
+
+  describe "Rules" do
+    before {visit rules_path }
+
+    it {should have_content('How to Play') }
+  end
+end
